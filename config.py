2747bc2ccde5005711701911930cf1e91096d0f3
pmgiral <pmgiral@gmail.com>
2026-02-11 14:25:57 +0100

    mas feeds

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ EMAIL_RECIPIENT = "pmgiral@pm.me"
 FEEDS = {
     "meneame": "https://www.meneame.net/rss",
     "pwned": "https://www.reddit.com/r/pwned/.rss",
+    "databreaches": "https://www.databreaches.net/feed/",
+    "haveibeenpwned": "https://feeds.feedburner.com/HaveIBeenPwnedLatestBreaches",
+    "ransomware": "https://www.ransomware.live/rss.xml"
 }
 
 KEYWORDS = ["cyber", "acronis", "vuln"]
